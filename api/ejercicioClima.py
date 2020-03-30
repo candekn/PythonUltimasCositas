64b61250a27a320d97e6c215304f47196f58b574
Duffy - <31010096+candekn@users.noreply.github.com>
2020-03-30 13:17:33 -0300

    Update ejercicioClima.py

--- api/ejercicioClima.py
+++ api/ejercicioClima.py
@@ -1,5 +1,6 @@
 import requests
-r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,AR&units=metric&appid=a41c27c9feb20967e253140e845e2322")
+#appid= apikey del clima
+r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,AR&units=metric&appid=")
 temp = r.json()["main"]["temp"]
 presion = r.json()["main"]["pressure"]
 humedad = r.json()["main"]["humidity"]
